6fa189b8160f4eef67e4740a5d4bd7eceb03b419
zolatso <tomsaunders1@gmail.com>
2025-01-15 09:01:52 +0100

    latest modifications

--- model.py
+++ model.py
@@ -10,48 +10,48 @@ class Player:
 
 
 class Tournament:
-    def __init__(self, name, place, start_date, end_date, number_of_rounds, current_round,
-                list_of_rounds, list_of_players, description):
+    def __init__(self, name, place, number_of_rounds, list_of_players,
+                description):
         self.name = name
         self.place = place
-        self.start_date = start_date 
-        self.end_date = end_date
-        self.number_of_rounds = number_of_rounds
-        self.current_round = current_round
-        self.list_of_rounds = list_of_rounds
+        self.start_date = datetime.datetime.now().date()
+        self.number_of_rounds = 4 if number_of_rounds is 0 else number_of_rounds
+        self.current_round = 0
         self.list_of_players = list_of_players
         self.description = description
 
+    def generate_new_round(self):
+        
+
 
 class Round:
-    def __init__(self, name, start_date, end_date):
+    def __init__(self, name, start_date):
         self.name = name
-        self.start_date = start_date
-        self.end_date = end_date
+        self.start_date = datetime.datetime.now().date()
 
 
 class Match:
     def __init__(self, ):
         pass
 
-def generate_random_pairs(list_of_players):
+
     
 
-def generate_pairs(list_of_players):
 
-def list_of_players():
+
+def extract_players(n):
     # This function takes the players json file and converts it into a list of player objects
     file_to_open = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/players/players.json')
     with open(file_to_open, 'r') as f:
         initial_list = json.load(f)
     converted_list = []
-    for person in initial_list:
-        converted_list.append(Player(person[0], person[1], person[2]))
+    for i in range(5):
+        converted_list.append(Player(initial_list[i][0], initial_list[i][1], initial_list[i][2]))
     return converted_list
 
 def main():
-    players = list_of_players()
-    print(players[10].last_name)
+    tournament80 = Tournament('egg', 'London', 5, extract_players(6), 'this is going to be a good tourny')
+    print(tournament80.list_of_players[5].first_name)
 
 
 if __name__ == '__main__':

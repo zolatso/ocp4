3fef7f6384b3e4de23bd85ed5f419f61a6dbfba7
ts1986 <tomsaunders1@gmail.com>
2025-01-20 10:26:41 +0100

    changes

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import datetime
-import random
+from random import choice, sample
 
 
 class Player:
@@ -23,10 +23,10 @@ class PlayerManager:
     def load(self):
         pass
 
-class PlayerInTournament:
-    def __init__(self, player):
-        self.player = player
-        self.score_in_tournament = 0
+#class PlayerInTournament:
+#    def __init__(self, player):
+#        self.player = player
+#        self.score_in_tournament = 0
 
 
 class Tournament:
@@ -36,25 +36,23 @@ class Tournament:
         self.start_date = datetime.datetime.now().date()
         self.number_of_rounds = kwargs.get('number_of_rounds', 4)
         self.current_round = 0
-        self.players = self.turn_players_into_tournament_players(kwargs['players'])
+        self.players = kwargs['players']
         self.description = kwargs['description']
     
-    def turn_players_into_tournament_players(self, players):
-        new_list = []
-        for obj in players:
-            new_list.append(PlayerInTournament(obj))
-            obj.tournaments.append(self)
-        return new_list
+    # def turn_players_into_tournament_players(self, players):
+    #     new_list = []
+    #     for obj in players:
+    #         new_list.append(PlayerInTournament(obj))
+    #         obj.tournaments.append(self)
+    #     return new_list
     
-    def create_first_matches(self, round, players):
+    def create_first_matches(self, players):
         matches = []
         players_copy = players.copy()
         while players_copy:
-            playerA = random.choice(players_copy)
-            players_copy.remove(playerA)
-            playerB = random.choice(players_copy)
-            players_copy.remove(playerB)
-            match = Match(self, round, playerA, 0, playerB, 0)
+            pair_of_players = sample(players_copy, 2)
+            match = ([pair_of_players[0], 0], [pair_of_players[1], 0])
+            players_copy.remove(pair_of_players)
             matches.append(match)
         return matches
     
@@ -66,11 +64,11 @@ class Tournament:
         name_of_round = "Round " + str(self.current_round)
         round = Round(name_of_round)
         if self.current_round == 1:
-            matches = self.create_first_matches(round, self.players)
+            matches = self.create_first_matches(self.players)
         else:
         # this functionality doesn't work yet as I haven't written the full pair generation function
         # simply repeats fully random generation of pairs
-            matches = self.create_first_matches(round, self.players)
+            matches = self.create_first_matches(self.players)
         round.matches = matches
         return round
 
@@ -94,43 +92,45 @@ class Round:
 
     def play_matches(self):
         for match in self.matches:
-            match.play_match()
+            possibilities = [1, 2, 3]
+            roll_the_dice = choice(possibilities)
+            score_a = match[0][1]
+            score_b = match[1][1]
+            if (roll_the_dice == 1):
+                score_a = 1
+                score_b = 0
+            elif (roll_the_dice == 2):
+                score_a = 0
+                score_b = 1
+            elif (roll_the_dice == 3):
+                score_a = 0.5
+                score_b = 0.5
 
 
-class Match:
-    """
-    Should not be a class, it may just be stocked as a tuple containing two lists, which any has two elements:
-    a player and a score
-    Ex: ([player_1, 0], [player_2, 1]) is a match
-    """
-    def __init__(self, tournament, round, playerA, playerA_score, playerB, playerB_score):
-        self.tournament = tournament
-        self.round = round
-        self.playerA = playerA
-        self.playerA_score = playerA_score
-        self.playerB = playerB
-        self.playerB_score = playerB_score
+#class Match:
+    # """
+    # Should not be a class, it may just be stocked as a tuple containing two lists, which any has two elements:
+    # a player and a score
+    # Ex: ([player_1, 0], [player_2, 1]) is a match
+    # """
+    # def __init__(self, tournament, round, playerA, playerA_score, playerB, playerB_score):
+    #     self.tournament = tournament
+    #     self.round = round
+    #     self.playerA = playerA
+    #     self.playerA_score = playerA_score
+    #     self.playerB = playerB
+    #     self.playerB_score = playerB_score
 
-    def play_match(self):
-        possibilities = [1, 2, 3]
-        roll_the_dice = random.choice(possibilities)
-        if (roll_the_dice == 1):
-            self.playerA_score = 1
-            self.playerB_score = 0
-        elif (roll_the_dice == 2):
-            self.playerA_score = 0
-            self.playerB_score = 1
-        elif (roll_the_dice == 3):
-            self.playerA_score = 0.5
-            self.playerB_score = 0.5
-        # These lines update both the tournament overall score and
-        # the player overall score
-        self.playerA.score_in_tournament += self.playerA_score
-        self.playerB.score_in_tournament += self.playerB_score
-        self.playerA.player.total_score += self.playerA_score
-        self.playerB.player.total_score += self.playerB_score
-        self.playerA.player.matches_played += 1
-        self.playerB.player.matches_played += 1
+    # def play_match(self):
+    #     
+    #     # These lines update both the tournament overall score and
+    #     # the player overall score
+    #     self.playerA.score_in_tournament += self.playerA_score
+    #     self.playerB.score_in_tournament += self.playerB_score
+    #     self.playerA.player.total_score += self.playerA_score
+    #     self.playerB.player.total_score += self.playerB_score
+    #     self.playerA.player.matches_played += 1
+    #     self.playerB.player.matches_played += 1
 
 def main():
     pass
